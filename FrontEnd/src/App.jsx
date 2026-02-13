2ee4040719cd8f85de9d5e6102ce783b1951cc4d
Vaibhav Thakare <154342566+VaibhavThakare124@users.noreply.github.com>
2026-02-13 09:35:54 +0530

    Update App.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+    const response = await axios.post('https://code-reviewer-backend-goml.onrender.com', { code })
     setReview(response.data)
   }
 
